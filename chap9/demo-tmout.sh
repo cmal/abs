f9b0e5b693f4d121910a944153fdaf944b0838f8
yuzhao <yu.zhao@joudou.com>
2017-07-26 14:41:42 +0800

    update

---
+++ chap9/demo-tmout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Works in scripts for Bash, versions 2.05b and later.
+
+TMOUT=3    # Prompt times out at three seconds.
+
+echo "What is your favorite song?"
+echo "Quickly now, you only have $TMOUT seconds to answer!"
+read song
+
+if [ -z "$song" ]
+then
+  song="(no answer)"
+  # Default response.
+fi
+
+echo "Your favorite song is $song."
